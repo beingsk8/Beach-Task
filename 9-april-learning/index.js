afb0a958bebc607d7b8a1ee4612b417daa066bbb
sourav <sourav.kumar1@tothenew.com>
2024-04-09 22:47:07 +0530

    changed

--- 9-april-learning/index.js
+++ 9-april-learning/index.js
@@ -1,9 +1,14 @@
 /*Blocking code */
+/*
 const fs = require('node:fs');
 const data = fs.readFileSync('/file.md'); // blocks here until file is read
+*/
 
 /*UnBlocking Code*/
+/*
 const fs = require('node:fs');
 fs.readFile('/file.md', (err, data) => {
     if (err) throw err;
-});
+});*/
+
+console.log("Hello World!");
