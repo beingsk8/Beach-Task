a81bda2e3d28336e068a2b77bbb00d7131778b46
sourav <sourav.kumar1@tothenew.com>
2024-04-09 17:09:36 +0530

    section 2 learning

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('commited')
+/*Blocking code */
+const fs = require('node:fs');
+const data = fs.readFileSync('/file.md'); // blocks here until file is read
+
+/*UnBlocking Code*/
+const fs = require('node:fs');
+fs.readFile('/file.md', (err, data) => {
+    if (err) throw err;
+});
